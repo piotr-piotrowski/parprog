40d48126ad8ba55ff6168c260971fede0c36fd82
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-04 22:05:03 +0100

    solution for ParallelCountChange

--- reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -41,22 +41,45 @@ object ParallelParenthesesBalancing {
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def check(chars: Array[Char], level: Int): Int =
+      if (chars.isEmpty) level
+      else if (chars.head == '(') check(chars.tail, level + 1)
+      else if (chars.head == ')')
+        if (level > 0)
+          check(chars.tail, level - 1)//if (level > 0) level - 1 else 1)
+        else -2
+      else check(chars.tail, level)
+    check(chars, 0) == 0
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) : (Int, Int) = {
+      def helper(chars: Array[Char], acc: (Int, Int)): (Int, Int) = {
+        if (chars.isEmpty) acc
+        else if (chars.head == '(') helper(chars.tail, (acc._1 + 1, acc._2))
+        else if (chars.head == ')') {
+          if (acc._1 > 0) helper(chars.tail, (acc._1 - 1, acc._2))
+          else helper(chars.tail, (acc._1, acc._2 + 1))
+        }
+        else helper(chars.tail, acc)
+      }
+      helper(chars.slice(idx, until), (arg1, arg2))
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val mid = (from + until) / 2
+        val (l, r) = parallel(reduce(from, mid), reduce(mid, until))
+        val matched = scala.math.min(l._1, r._2)
+        (l._1 + r._1 - matched, l._2 + r._2 - matched)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
