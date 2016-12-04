c0f1069cefdf7300d2f162ed2a37090b6cc392fe
Piotr Piotrowski <piotr.j.piotrowski@gmail.com>
2016-12-04 18:52:05 +0100

    solution for ParallelCountChange

--- reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -46,7 +46,10 @@ object ParallelCountChange {
    *  coins for the specified amount of money.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
-    ???
+    if (money == 0) 1
+    else if (money > 0 && coins.nonEmpty)
+      countChange(money - coins.head, coins) + countChange(money, coins.tail)
+    else 0
   }
 
   type Threshold = (Int, List[Int]) => Boolean
@@ -55,20 +58,24 @@ object ParallelCountChange {
    *  specified list of coins for the specified amount of money.
    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    ???
+    if (threshold(money, coins) || money <= 0 || coins.isEmpty) countChange(money, coins)
+    else {
+      val (left, right) =
+        parallel[Int, Int] (
+          parCountChange(money - coins.head, coins, threshold)
+          , parCountChange(money, coins.tail, threshold))
+      left + right
+    }
   }
 
   /** Threshold heuristic based on the starting money. */
-  def moneyThreshold(startingMoney: Int): Threshold =
-    ???
+  def moneyThreshold(startingMoney: Int): Threshold = (money, coins) => money <= (2 * startingMoney) / 3
 
   /** Threshold heuristic based on the total number of initial coins. */
-  def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ???
+  def totalCoinsThreshold(totalCoins: Int): Threshold = (money, coins) => coins.length <= (2 * totalCoins) / 3
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
-  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ???
-  }
+  def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = (money, coins) =>
+    (money * coins.length) <= ((startingMoney * allCoins.length) / 2)
 }
